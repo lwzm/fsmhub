267460530a124d2eef19e8e8a357aad6aff0a0b6
lwzm <lwzm@qq.com>
2019-10-05 21:59:12 +0800

    fix StopIteration

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ name = "fsm-hub"
 
 setup(
     name=name,
-    version="1.11",
+    version="1.12",
     description="Finite State Machine storage hub",
     author=author,
     author_email="{}@qq.com".format(author),
