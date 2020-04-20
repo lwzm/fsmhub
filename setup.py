d764d6255f2b4dd65d1f3793af4a814bd528f5ca
lwzm <lwzm@qq.com>
2020-04-20 20:37:49 +0800

    v3.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ name = "fsm-hub"
 
 setup(
     name=name,
-    version="3.1",
+    version="3.2",
     description="Finite State Machine storage hub",
     author=author,
     author_email="{}@qq.com".format(author),
