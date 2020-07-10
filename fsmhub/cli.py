37703503021c852aeb97dd8b3323adf9118555b6
lwzm <lwzm@qq.com>
2020-07-10 18:02:17 +0800

    new cli

--- fsmhub/cli.py
+++ fsmhub/cli.py
@@ -1,39 +1,56 @@
 #!/usr/bin/env python3
 
-from os import environ
-from requests import Session, codes
+import json
+
+from http import client
+from urllib.parse import urlparse
 
 
-hub = environ.get("HUB", "http://localhost:1024")
-http = Session()
+class HTTPError(Exception):
+    pass
 
 
-def get(id):
-    rsp = http.get(f"{hub}/{id}")
-    rsp.raise_for_status()
-    return rsp.json()
+class Api:
+    def __init__(self, url):
+        url = urlparse(url)
+        if url.scheme == "http":
+            self.http = client.HTTPConnection(url.netloc)
+        elif url.scheme == "https":
+            self.http = client.HTTPSConnection(url.netloc)
+        else:
+            raise ValueError(url)
 
+    def get(self, id):
+        self.http.request("GET", f"/{id}")
+        r = self.http.getresponse()
+        if r.status > client.NO_CONTENT:
+            raise HTTPError(r.status)
+        return json.load(r)
 
-def new(state, initial_data: dict = None):
-    rsp = http.post(f"{hub}/new/{state}", json=initial_data)
-    rsp.raise_for_status()
+    def _post(self, path, data=None):
+        body = data and json.dumps(data)
+        self.http.request("POST", path, body, headers={"Content-Type": "application/json"})
+        r = self.http.getresponse()
+        if r.status > client.NO_CONTENT:
+            raise HTTPError(r.status)
+        o = r.read()
+        if o:
+            return json.loads(o)
 
+    def new(self, state, initial_data: dict = None):
+        return self._post(f"/new/{state}", initial_data)
 
-def lock(state):
-    rsp = http.post(f"{hub}/lock/{state}?wait=yes")
-    if rsp.status_code == codes.NOT_FOUND:
-        return
-    rsp.raise_for_status()  # hub is gone or logic is broken, so just exit
-    return rsp.json()
+    def lock(self, state):
+        return self._post(f"/lock/{state}?wait=yes")
 
+    def transit(self, id, next_state, patch_data: dict = None):
+        return self._post(f"/transit/{id}/{next_state}", patch_data)
 
-def transit(id, next_state, patch_data: dict = None):
-    rsp = http.post(f"{hub}/transit/{id}/{next_state}", json=patch_data)
-    rsp.raise_for_status()
 
 
 if __name__ == '__main__':
-    new("x")
-    id = lock("x")["id"]
-    transit(id, "xxx")
-    print(get(id))
+    api = Api("http://localhost:8000")
+    api.new("x", {"a": 1})
+    id = api.lock("x")["id"]
+    api.transit(id, "xxx", {"a": 2})
+    print(api.get(id))
