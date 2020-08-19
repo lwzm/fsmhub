5e799b940989339bc47e34ceac06050cc1890b20
lwzm <lwzm@qq.com>
2020-08-19 15:37:37 +0800

    some improved features

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ author = "lwzm"
 
 setup(
     name="fsmhub",
-    version="4.5",
+    version="4.6",
     description="Finite State Machine storage hub",
     author=author,
     author_email="{}@qq.com".format(author),
